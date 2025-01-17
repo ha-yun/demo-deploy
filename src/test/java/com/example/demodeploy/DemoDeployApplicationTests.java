1326e5f182af8ad89ffa763aeade242f38b22476
ha-yun <deu05230@naver.com>
2025-01-17 17:07:31 +0900

    test  error 수정

--- src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
+++ src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoDeployApplicationTests {
 
-    @Test
-    void contextLoads() {
+    //@Test
+    //void contextLoads() {
         // 단위 테스트
         // 에러 강제 발생 시킴 -> 중단여부 체크
         // 테스트 실패되면 반영X ( git에서는 반영되지만, 배포 및 운영에서는 반영 X )
@@ -17,6 +17,6 @@ class DemoDeployApplicationTests {
         // RestAPI 테스트 가능
         // 메세지가 보이지는 않지만, 차후 코드로 확인
         // System.out.println("단위 테스트 호출!! echo");
-    }
+    //}
 
 }
